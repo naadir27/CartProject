8240a3e1ca47be4a8a83e7a6b2319597003e8920
Naadir Mohamed <smartnaadir@gmail.com>
2023-02-23 22:02:25 +0400

    PWA committed

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ var cacheFiles = [
     'images/icon.png'
     ];
 
-self.addEventListener('install',(e) =>{
+self.addEventListener('install',(e) => {
     console.log('[Service Worker] Install');
     e.waitUntil(
         caches.open(cacheName).then((cache) =>{
@@ -14,4 +14,20 @@ self.addEventListener('install',(e) =>{
         return cache.addAll(cacheFiles);
     })
     );
+});
+
+self.addEventListener('fetch', function(e) { 
+    e.respondWith( 
+        caches.match(e.request).then(function (r) {
+            console.log('[Service Worker] Fetched resource '+ e.request.url); 
+            // Download the file if it is not in the cache,
+            return r || fetch(e.request).then(function(response) {
+                // add the new file to cache
+                return caches.open(cacheName).then(function (cache){ 
+                    cache.put(e.request, response.clone());
+                    return response; 
+                }); 
+            }); 
+        }) 
+    ); 
 });
