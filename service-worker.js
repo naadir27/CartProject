e6b61f5fddab1953fe4c188407b4ee7b6300068a
Naadir Mohamed <smartnaadir@gmail.com>
2023-02-23 21:39:18 +0400

    Changes

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@ var cacheFiles = [
 self.addEventListener('install',(e) =>{
     console.log('[Service Worker] Install');
     e.waitUntil(
-        cache.open(cacheName).then((cache) =>{
+        caches.open(cacheName).then((cache) =>{
         console.log('[Service Worker] Caching all the files');
         return cache.addAll(cacheFiles);
     })
